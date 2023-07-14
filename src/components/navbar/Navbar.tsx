bc66f6c1314ea6f1a7e97d1c9d80d883a48ae80a
Igor Vinicius <igorviniciusf10@gmail.com>
2023-07-14 16:22:05 -0300

    Corrigir quebras de linha

--- src/components/navbar/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import "./style.scss"
-
-export default function Navbar(){
-    return(
-        <nav>
-<h2>navbar</h2>
-        </nav>
-    )
-}
--- src/pages/home/Home.tsx
+++
@@ -1,9 +0,0 @@
-import "./style.scss"
-
-export default function Home(){
-    return(
-        <div className="container">
-            <h2>ola</h2>
-        </div>
-    )
-}
