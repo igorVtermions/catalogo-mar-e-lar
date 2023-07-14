ad0956b056518da6c9556e1d04267fd9a5ce4d86
Igor Vinicius <igorviniciusf10@gmail.com>
2023-07-14 16:28:35 -0300

    Corrigir quebras de linha

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import "../src/styles/global.scss"
+
+export default function App() {
+  
+
+  return (
+    <>
+    <Navbar />
+      <Home />
+    </>
+  )
+}
+
+
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+import logo from "../../assets/logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={logo} alt="Logo Mar e Lar" />
+    </nav>
+  );
+}
