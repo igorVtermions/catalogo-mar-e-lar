54016fd0875be264a6b17b7a9e493e81c212c6f9
Igor Vinicius <igorviniciusf10@gmail.com>
2023-07-21 10:16:17 -0300

    Suas mensagens de commit aqui

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,6 @@ type ListItem = {
   id: number;
   name: string;
   alt: string;
-  description: string;
   price: string;
   photo1: string;
   photo2: string;
@@ -14,12 +13,11 @@ export default function Home() {
   const List: ListItem[] = [
     {
       id: 1,
-      name: "produto 1",
+      name: "ombrelone de 2,40mt (S/Aba)",
       alt: "Ombrelone de 2,40mt",
-      price: "R$ 200",
-      description: "produto de 240 metros",
+      price: "R$ 370,00",
       photo1:
-        "https://cdn.discordapp.com/attachments/1037425403985399841/1119234277817729064/servicos.jpg",
+        "https://cdn.discordapp.com/attachments/1037425403985399841/1130556033996177468/ombrelone-redondo-2mt.jpg",
       photo2:
         "https://cdn.discordapp.com/attachments/1037425403985399841/1116066887080620113/post-10.jpg",
     },
@@ -28,23 +26,24 @@ export default function Home() {
       name: "produto 2",
       alt: "Ombrelone Quadrado 1,60mt",
       price: "R$ 100",
-      description: "produto de 640 metros",
       photo1:
-        "https://cdn.discordapp.com/attachments/1037425403985399841/1113546723101511821/post-08.jpg",
+        "https://cdn.discordapp.com/attachments/1037425403985399841/1130556033580929115/ombrelone-quadrado-branco.jpg",
       photo2:
         "https://cdn.discordapp.com/attachments/1037425403985399841/1112791642391445574/post-06.jpg",
     },
   ];
 
   const roadItem = List.map((item: ListItem) => (
-    <div key={item.id}>
-      <img src={item.photo1} alt={item.alt} />
-      <img src={item.photo2} alt={item.alt} />
+    <div key={item.id} className="container">
+      <img src={item.photo1} alt={item.alt} className="fistImg" />
       <h1>{item.name}</h1>
       <h2>{item.price}</h2>
-      <p>{item.description}</p>
     </div>
   ));
 
-  return <div className="container">{roadItem}</div>;
+  return (
+    <section>
+      {roadItem}
+    </section>
+  )
 }
